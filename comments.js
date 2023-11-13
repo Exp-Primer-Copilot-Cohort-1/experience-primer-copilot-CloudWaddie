a0021a013002d892cbdf735cbf71a171c8305bc6
CloudWaddie <148834837+CloudWaddie@users.noreply.github.com>
2023-11-13 06:00:13 +0000

    Refactor comments.js file by removing unnecessary code and improving readability.

--- comments.js
+++ comments.js
@@ -1,88 +1,51 @@
-// Create Web Server
+// Create web server
 var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-
-// Create Database Connection
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/comment');
-
-// Create Schema
-var commentSchema = mongoose.Schema({
-  _id: Number,
-  name: String,
-  comment: String,
-  date: { type: Date, default: Date.now },
-  parent: Number
-});
-
-// Create Model
-var Comment = mongoose.model('Comment', commentSchema);
-
-// Create Server
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// Set Port
-var port = process.env.PORT || 8080;
-
-// Set Router
 var router = express.Router();
+// Create database connection
+var connection = require('./connectDatabase.js');
 
-// Middleware for all request
-router.use(function(req, res, next) {
-  console.log('Something is happening.');
-  next();
-});
-
-// Create Comment
-router.route('/comments').post(function(req, res) {
-  var comment = new Comment();
-  comment._id = req.body._id;
-  comment.name = req.body.name;
-  comment.comment = req.body.comment;
-  comment.parent = req.body.parent;
-
-  comment.save(function(err) {
-    if (err) res.send(err);
-    res.json({ message: 'Comment created!' });
-  });
-});
-
-// Get All Comments
-router.route('/comments').get(function(req, res) {
-  Comment.find(function(err, comments) {
-    if (err) res.send(err);
-    res.json(comments);
-  });
-});
-
-// Get Comment By ID
-router.route('/comments/:comment_id').get(function(req, res) {
-  Comment.findById(req.params.comment_id, function(err, comment) {
-    if (err) res.send(err);
-    res.json(comment);
-  });
-});
-
-// Update Comment By ID
-router.route('/comments/:comment_id').put(function(req, res) {
-  Comment.findById(req.params.comment_id, function(err, comment) {
-    if (err) res.send(err);
-    comment._id = req.body._id;
-    comment.name = req.body.name;
-    comment.comment = req.body.comment;
-    comment.parent = req.body.parent;
-
-    comment.save(function(err) {
-      if (err) res.send(err);
-      res.json({ message: 'Comment updated!' });
+// Get comments from database
+router.get('/', function(req, res) {
+    connection.query('SELECT * FROM comments', function(err, rows, fields) {
+        if (err) {
+            console.log(err);
+        }
+        res.json(rows);
     });
-  });
 });
 
-// Delete Comment By ID
-router.route('/comments/:comment_id').delete(function(req, res) {
-  Comment.remove(
-    {
-      _id:
+// Add comments to database
+router.post('/', function(req, res) {
+    var comment = req.body;
+    connection.query('INSERT INTO comments SET ?', comment, function(err, result) {
+        if (err) {
+            console.log(err);
+        }
+        res.json(result);
+    });
+});
+
+// Delete comments from database
+router.delete('/:id', function(req, res) {
+    var id = req.params.id;
+    connection.query('DELETE FROM comments WHERE id = ?', id, function(err, result) {
+        if (err) {
+            console.log(err);
+        }
+        res.json(result);
+    });
+});
+
+// Update comments in database
+router.put('/:id', function(req, res) {
+    var id = req.params.id;
+    var comment = req.body;
+    connection.query('UPDATE comments SET ? WHERE id = ?', [comment, id], function(err, result) {
+        if (err) {
+            console.log(err);
+        }
+        res.json(result);
+    });
+});
+
+module.exports = router;
